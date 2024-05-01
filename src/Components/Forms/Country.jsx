6b859d5b83aecda215af65882f15139d6379922e
Your Name <your_email@example.com>
2024-05-01 22:43:43 +0530

    first commit

---
+++ src/Components/Forms/Country.jsx
@@ -0,0 +1,77 @@
+import React, { useState } from 'react';
+import "./personalData.css";
+const countries = [
+  { name: 'Afghanistan', code: 'AF' },
+  { name: 'Albania', code: 'AL' },
+  { name: 'Åland Islands', code: 'AX' },
+  { name: 'Argentina', code: 'AR' },
+  { name: 'Australia', code: 'AU' },
+  { name: 'Bangladesh', code: 'BD' },
+  { name: 'Belgium', code: 'BE' },
+  { name: 'Brazil', code: 'BR' },
+  { name: 'Canada', code: 'CA' },
+  { name: 'Chile', code: 'CL' },
+  { name: 'China', code: 'CN' },
+  { name: 'Colombia', code: 'CO' },
+  { name: 'Denmark', code: 'DK' },
+  { name: 'Egypt', code: 'EG' },
+  { name: 'Finland', code: 'FI' },
+  { name: 'France', code: 'FR' },
+  { name: 'Germany', code: 'DE' },
+  { name: 'Greece', code: 'GR' },
+  { name: 'India', code: 'IN' },
+  { name: 'Indonesia', code: 'ID' },
+  { name: 'Iran', code: 'IR' },
+  { name: 'Ireland', code: 'IE' },
+  { name: 'Israel', code: 'IL' },
+  { name: 'Italy', code: 'IT' },
+  { name: 'Japan', code: 'JP' },
+  { name: 'Malaysia', code: 'MY' },
+  { name: 'Mexico', code: 'MX' },
+  { name: 'Morocco', code: 'MA' },
+  { name: 'Netherlands', code: 'NL' },
+  { name: 'New Zealand', code: 'NZ' },
+  { name: 'Nigeria', code: 'NG' },
+  { name: 'Norway', code: 'NO' },
+  { name: 'Pakistan', code: 'PK' },
+  { name: 'Peru', code: 'PE' },
+  { name: 'Philippines', code: 'PH' },
+  { name: 'Poland', code: 'PL' },
+  { name: 'Portugal', code: 'PT' },
+  { name: 'Russia', code: 'RU' },
+  { name: 'Saudi Arabia', code: 'SA' },
+  { name: 'South Africa', code: 'ZA' },
+  { name: 'South Korea', code: 'KR' },
+  { name: 'Spain', code: 'ES' },
+  { name: 'Sweden', code: 'SE' },
+  { name: 'Switzerland', code: 'CH' },
+  { name: 'Thailand', code: 'TH' },
+  { name: 'Turkey', code: 'TR' },
+  { name: 'Ukraine', code: 'UA' },
+  { name: 'United Arab Emirates', code: 'AE' },
+  { name: 'United Kingdom', code: 'GB' },
+  { name: 'United States', code: 'US' },
+  { name: 'Vietnam', code: 'VN' }
+];
+
+const CountrySelect = () => {
+  const [selectedCountry, setSelectedCountry] = useState(countries[0]);
+
+  const handleChange = (event) => {
+    const selectedCode = event.target.value;
+    const selected = countries.find((country) => country.code === selectedCode);
+    setSelectedCountry(selected);
+  };
+
+  return (
+    <select id='country_select' value={selectedCountry.code} onChange={handleChange}>
+      {countries.map((country) => (
+        <option key={country.code} value={country.code}>
+          {country.name}
+        </option>
+      ))}
+    </select>
+  );
+};
+
+export default CountrySelect;
